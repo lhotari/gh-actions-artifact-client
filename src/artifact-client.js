a2cafa23c1a58ae983a87086fbd5f23383c0cfe5
Lari Hotari <lari@hotari.net>
2022-03-26 22:15:52 +0200

    Use yargs properly

--- src/artifact-client.js
+++ src/artifact-client.js
@@ -1,39 +1,29 @@
-const argv = require('yargs')
-  .usage('$0 <artifactName>')
-  .option('upload', {
-    alias: 'u',
-    description: 'upload',
-    type: 'boolean'
-  })
-  .option('download', {
-    alias: 'd',
-    description: 'download',
-    type: 'boolean'
-  })
-  .option('retentionDays', {
-    alias: 'r',
-    description: 'retention days',
-    type: 'number',
-    default: 1
-  })
-  .positional('artifactName', {
-    type: 'string',
-    describe: 'artifact name',
-    demandOption: 'true'
+require('yargs')
+  .command({
+    command: 'upload <artifactName>',
+    desc: 'upload an artifact',
+    builder: yargs =>
+      yargs
+        .positional('artifactName', {
+          type: 'string',
+          describe: 'artifact name'
+        })
+        .option('retentionDays', {
+          alias: 'r',
+          description: 'retention days',
+          type: 'number',
+          default: 1
+        }),
+    handler: argv => {
+      const artifactName = argv.artifactName
+      console.log('artifactName', artifactName)
+      const ExtendedUploadHttpClient = require('./upload-http-client.js')
+      const uploadHttpClient = new ExtendedUploadHttpClient()
+      uploadHttpClient.uploadStream(artifactName, process.stdin, {
+        retentionDays: argv.retentionDays
+      })
+    }
   })
   .help()
-  .alias('help', 'h').argv
-
-const artifactName = argv.artifactName
-if (argv.upload) {
-  const ExtendedUploadHttpClient = require('./upload-http-client.js')
-  console.log(ExtendedUploadHttpClient)
-  const uploadHttpClient = new ExtendedUploadHttpClient()
-  uploadHttpClient.uploadStream(artifactName, process.stdin, {
-    retentionDays: argv.retentionDays
-  })
-} else if (argv.download) {
-  console.error('Not implemented.')
-} else {
-  console.error('Must specify --upload or --download.')
-}
+  .alias('help', 'h')
+  .argv
