4384b260f47e3ed297de6616e279208c3147101a
Lari Hotari <lhotari@users.noreply.github.com>
2024-10-16 15:27:08 +0300

    Reformat

--- install.js
+++ install.js
@@ -13,34 +13,40 @@ async function run() {
     localBinPath,
     'gh-actions-artifact-client.js'
   )
-  let clientJsContent;
+  let clientJsContent
   try {
     // If dist/index.js exists, use it
     clientJsContent = fs.readFileSync(
       path.resolve(__dirname, 'dist/index.js'),
       'UTF-8'
-    );
+    )
   } catch (error) {
     if (error.code === 'ENOENT') {
       // If dist/index.js doesn't exist, look for index.js
       clientJsContent = fs.readFileSync(
         path.resolve(__dirname, 'index.js'),
         'UTF-8'
-      );
+      )
     } else {
-      throw error;
+      throw error
     }
   }
   // Embed a snapshot of the required environment variables to the gh-actions-artifact-client.js file
-  fs.writeFileSync(clientJsPath, `#!/usr/bin/env node
+  fs.writeFileSync(
+    clientJsPath,
+    `#!/usr/bin/env node
 process.env.ACTIONS_RUNTIME_URL='${process.env.ACTIONS_RUNTIME_URL}';
 process.env.ACTIONS_RUNTIME_TOKEN='${process.env.ACTIONS_RUNTIME_TOKEN}';
 process.env.ACTIONS_RESULTS_URL='${process.env.ACTIONS_RESULTS_URL}';
-${clientJsContent}`, {
-    encoding: 'UTF-8',
-    mode: '755'
-  })
-  console.log(`Copied index.js as executable ${clientJsPath} with embedded ACTIONS_RUNTIME_URL, ACTIONS_RESULTS_URL and ACTIONS_RUNTIME_TOKEN environment variables copied from environment.`)
+${clientJsContent}`,
+    {
+      encoding: 'UTF-8',
+      mode: '755'
+    }
+  )
+  console.log(
+    `Copied index.js as executable ${clientJsPath} with embedded ACTIONS_RUNTIME_URL, ACTIONS_RESULTS_URL and ACTIONS_RUNTIME_TOKEN environment variables copied from environment.`
+  )
   fs.appendFileSync(process.env.GITHUB_PATH, `${localBinPath}\n`)
   console.log(`Added ${localBinPath} to GITHUB_PATH`)
 }
